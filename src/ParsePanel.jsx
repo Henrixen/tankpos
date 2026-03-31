b394b765decaec943deae55831ac1616521d36c8
Henrixen <haakonhenriksen@gmail.com>
2026-04-01 00:58:53 +0200

    Update ParsePanel.jsx

--- src/ParsePanel.jsx
+++ src/ParsePanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import * as XLSX from "xlsx";
 import { C } from "./constants";
 import { loadImg, normaliseQty, rollOpenDateForward } from "./utils";
