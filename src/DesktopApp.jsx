96fa5ee360d61736eede687ae8a8ae6f538a1a3b
Henrixen <haakonhenriksen@gmail.com>
2026-04-09 23:05:59 +0200

    Update DesktopApp.jsx

--- src/DesktopApp.jsx
+++ src/DesktopApp.jsx
@@ -286,7 +286,7 @@ const filtV=useMemo(()=>{
               
               {/* LEFT: Parse + Fixing (32%) */}
               <div style={{width:mobile?"100%":"32%",display:"flex",flexDirection:"column",gap:10}}>
-  <div style={{minHeight:180,maxHeight:600,resize:"vertical",overflow:"auto"}}><ParsePanel vessels={vessels} onAddVessels={onAddVessels} onAddCargoes={onAddCargoes} lockedMode="pos" vesselDB={{}}/></div>
+  <div><ParsePanel vessels={vessels} onAddVessels={onAddVessels} onAddCargoes={onAddCargoes} lockedMode="pos" vesselDB={{}}/></div>
   <div><FixingWindow vessels={filtV} opFilter={opFilter} onOpFilter={op=>setOpFilter(o=>o===op?null:op)} /></div>
 </div>
  
